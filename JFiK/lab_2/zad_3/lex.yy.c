664ef9966d226acd8b984b49f375b2c6a1fbf43d
qwercik <erykandrzejewski2000@gmail.com>
2021-05-08 16:10:21 +0200

    Add more solutions

--- JFiK/lab_2/zad_3/lex.yy.c
+++ JFiK/lab_2/zad_3/lex.yy.c
@@ -370,8 +370,8 @@ static void yynoreturn yy_fatal_error ( const char* msg  );
 		YY_FATAL_ERROR( "token too large, exceeds YYLMAX" ); \
 	yy_flex_strncpy( yytext, (yytext_ptr), yyleng + 1 ); \
 	(yy_c_buf_p) = yy_cp;
-#define YY_NUM_RULES 5
-#define YY_END_OF_BUFFER 6
+#define YY_NUM_RULES 4
+#define YY_END_OF_BUFFER 5
 /* This struct is not used in this scanner,
    but its presence is necessary. */
 struct yy_trans_info
@@ -379,10 +379,9 @@ struct yy_trans_info
 	flex_int32_t yy_verify;
 	flex_int32_t yy_nxt;
 	};
-static const flex_int16_t yy_accept[12] =
+static const flex_int16_t yy_accept[8] =
     {   0,
-        0,    0,    0,    0,    6,    2,    5,    1,    4,    3,
-        0
+        0,    0,    5,    3,    2,    1,    0
     } ;
 
 static const YY_CHAR yy_ec[256] =
@@ -390,12 +389,12 @@ static const YY_CHAR yy_ec[256] =
         1,    1,    1,    1,    1,    1,    1,    1,    1,    2,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    3,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    3,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
 
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
@@ -422,34 +421,30 @@ static const YY_CHAR yy_meta[4] =
         1,    1,    1
     } ;
 
-static const flex_int16_t yy_base[13] =
+static const flex_int16_t yy_base[8] =
     {   0,
-        0,    0,    5,    4,    5,    8,    8,    8,    8,    8,
-        8,    3
+        0,    0,    4,    5,    5,    5,    5
     } ;
 
-static const flex_int16_t yy_def[13] =
+static const flex_int16_t yy_def[8] =
     {   0,
-       11,    1,   12,   12,   11,   11,   11,   11,   11,   11,
-        0,   11
+        7,    1,    7,    7,    7,    7,    0
     } ;
 
-static const flex_int16_t yy_nxt[12] =
+static const flex_int16_t yy_nxt[9] =
     {   0,
-        6,    7,    8,    9,   11,   10,   10,    5,   11,   11,
-       11
+        4,    5,    6,    7,    3,    7,    7,    7
     } ;
 
-static const flex_int16_t yy_chk[12] =
+static const flex_int16_t yy_chk[9] =
     {   0,
-        1,    1,    1,   12,    5,    4,    3,   11,   11,   11,
-       11
+        1,    1,    1,    3,    7,    7,    7,    7
     } ;
 
 /* Table of booleans, true if rule could match eol. */
-static const flex_int32_t yy_rule_can_match_eol[6] =
+static const flex_int32_t yy_rule_can_match_eol[5] =
     {   0,
-0, 0, 1, 0, 0,     };
+0, 1, 0, 0,     };
 
 static yy_state_type yy_last_accepting_state;
 static char *yy_last_accepting_cpos;
@@ -472,15 +467,17 @@ char yytext[YYLMAX];
 char *yytext_ptr;
 #line 1 "scan.l"
 #line 2 "scan.l"
+  // To jest dość uproszczona wersja zadania, bo nie wiem czy chodzi mu tylko o trójkąt z przeciwprostokątną po prawej stronie, czy może także po lewej
   #include <stdio.h>
   int yywrap(void);
   int yylex(void);
+  int current_in_row = 0;
+  int should_be_in_row = 1;
+  int err = 0;
+#line 478 "lex.yy.c"
 #line 479 "lex.yy.c"
 
-#line 481 "lex.yy.c"
-
 #define INITIAL 0
-#define DALEJ 1
 
 #ifndef YY_NO_UNISTD_H
 /* Special case for "unistd.h", since it is non-ANSI. We include it way
@@ -695,9 +692,9 @@ YY_DECL
 		}
 
 	{
-#line 7 "scan.l"
+#line 10 "scan.l"
 
-#line 701 "lex.yy.c"
+#line 698 "lex.yy.c"
 
 	while ( /*CONSTCOND*/1 )		/* loops until end-of-file is reached */
 		{
@@ -724,13 +721,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 12 )
+				if ( yy_current_state >= 8 )
 					yy_c = yy_meta[yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 8 );
+		while ( yy_base[yy_current_state] != 5 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -766,33 +763,34 @@ do_action:	/* This label is used only to access EOF actions. */
 
 case 1:
 YY_RULE_SETUP
-#line 8 "scan.l"
-;
+#line 11 "scan.l"
+current_in_row++;
 	YY_BREAK
 case 2:
+/* rule 2 can match eol */
 YY_RULE_SETUP
-#line 9 "scan.l"
-{ putchar(*yytext); BEGIN DALEJ; }
+#line 12 "scan.l"
+{
+    if (current_in_row != should_be_in_row) {
+        err = 1;
+    }
+
+    should_be_in_row++;
+    current_in_row = 0;
+}
 	YY_BREAK
 case 3:
-/* rule 3 can match eol */
 YY_RULE_SETUP
-#line 10 "scan.l"
-{ putchar('\n'); BEGIN INITIAL; }
+#line 20 "scan.l"
+err = 1;
 	YY_BREAK
 case 4:
 YY_RULE_SETUP
-#line 11 "scan.l"
-putchar(*yytext);
-	YY_BREAK
-case 5:
-YY_RULE_SETUP
-#line 12 "scan.l"
+#line 24 "scan.l"
 ECHO;
 	YY_BREAK
-#line 794 "lex.yy.c"
+#line 793 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
-case YY_STATE_EOF(DALEJ):
 	yyterminate();
 
 	case YY_END_OF_BUFFER:
@@ -1088,7 +1086,7 @@ static int yy_get_next_buffer (void)
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 12 )
+			if ( yy_current_state >= 8 )
 				yy_c = yy_meta[yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
@@ -1116,11 +1114,11 @@ static int yy_get_next_buffer (void)
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 12 )
+		if ( yy_current_state >= 8 )
 			yy_c = yy_meta[yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
-	yy_is_jam = (yy_current_state == 11);
+	yy_is_jam = (yy_current_state == 7);
 
 		return yy_is_jam ? 0 : yy_current_state;
 }
@@ -1808,8 +1806,18 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 12 "scan.l"
+#line 24 "scan.l"
 
 int yywrap(void) { return 1; }
-int main(void) { return yylex(); }
+int main(void) {
+    int result = yylex();
+
+    if (err) {
+        puts("yylex:Error!");
+    } else {
+        puts("OK!");
+    }
+
+    return result;
+}
 
